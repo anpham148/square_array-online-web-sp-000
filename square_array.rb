165bce3d872071a7830927dce4a8b23632bac190
An Pham <anpham148@gmail.com>
2020-06-22 09:20:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   # your code here
   new_array = []
   array.each do |number|
-    new_array.push(number*number)
+    new_number = number*number
+    new_array.push(new_array)
   end
 end
